2cdfd738c13c442fa26f00010d204406467fda4c
Sirorat <srr.py2004@gmail.com>
2026-04-01 11:35:58 +0700

    Edit Registration form - calculate weekly per rate and return popup message

--- payRateCal.js
+++ payRateCal.js
@@ -2,9 +2,20 @@ function payRateCalculate() {
   let workDays = (document.getElementById("workDays").value).trim();
   let selectObj = document.getElementById("year");
   let yearOfStudy = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  //alert("yearOfStudy = "+yearOfStudy);
   
-  return 0;
+  let ratePerHour;
+  if(yearOfStudy === 1){
+    ratePerHour = 55;
+  }else if(yearOfStudy === 2){
+    ratePerHour = 65;
+  }else if(yearOfStudy === 3){
+    ratePerHour = 75;
+  }else if(yearOfStudy >= 4){
+    ratePerHour = 85
+  }
+  
+  let total = ratePerHour * 8 * workDays;
+  return total;
 }
 
 
